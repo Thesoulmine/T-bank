5f8ea2f33159f7bb2db8607c65fc212443b66213
Thesoulmine <mohk223@gmail.com>
2024-12-01 10:25:24 +0300

    Add logs

--- logs/build.gradle.kts
+++ logs/build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
